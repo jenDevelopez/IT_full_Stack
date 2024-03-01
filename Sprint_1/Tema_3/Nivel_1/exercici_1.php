43661b247814c4af23165689039348b9b2166c52
Jennifer López <jen1992lo@gmail.com>
2024-03-01 10:52:33 +0100

    Nivel 1/Ejercicio 1 finalizado

---
+++ Sprint_1/Tema_3/Nivel_1/exercici_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nivel 1 Exercici 1</title>
+</head>
+<body>
+  <?php
+    $numbers = array(1,2,3,4,5);
+
+    foreach ($numbers as $number) {
+      echo $number ." ";
+    }
+  ?>
+</body>
+</html>
