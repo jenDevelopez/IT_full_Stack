a7dbfdd05d501ccf710ad2118ba9b161aa3010a1
Jennifer <jen1992lo@gmail.com>
2024-03-28 17:10:53 +0100

    Tema 5/Nivel 3/ejercicio 1 terminado

--- Sprint_1/Tema_5/Nivel_2/exercici_1.php
+++ Sprint_1/Tema_5/Nivel_2/exercici_1.php
@@ -1,9 +1,9 @@
 <?php
 abstract class Shape{
   protected $ample;
-  protected $alt;
+  protected $alt = null;
 
-   function __construct($ample,$alt) {
+   function __construct($ample,$alt = null) {
     $this->ample = $ample;
     $this->alt = $alt;
   }
@@ -22,7 +22,15 @@ class Rectangle extends Shape{
   }
 }
 
+class Circle extends Shape{
+  public function area():float {
+    $r = $this->ample / 2;
+    return M_PI * pow($r,2);
+  }
+}
 $triangle = new Triangle(8,6);
 echo $triangle->area() ."\n";
 $rectangle = new Rectangle(8,6);
 echo $rectangle->area()."\n";
+$circle = new Circle(8);
+echo $circle->area() ."\n";
