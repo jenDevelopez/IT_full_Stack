59885743cef1558fe575a43520eec519bf0517ce
Jennifer <jen1992lo@gmail.com>
2024-03-28 20:41:28 +0100

    Tema 5/Nivel 3/ejercicio 1 terminado

---
+++ Sprint_1/Tema_5/Nivel_3/exercici_1.php
@@ -0,0 +1,29 @@
+<?php
+abstract class Shape{
+  protected $ample;
+  protected $alt = null;
+
+   function __construct($ample,$alt = null) {
+    $this->ample = $ample;
+    $this->alt = $alt;
+  }
+  abstract public function area():float;
+}
+
+class Triangle extends Shape{
+  public function area():float {
+     return ($this->ample * $this->alt) / 2;
+  }
+}
+
+class Rectangle extends Shape{
+  public function area():float {
+     return $this->ample * $this->alt;
+  }
+}
+
+
+$triangle = new Triangle(8,6);
+echo $triangle->area() ."\n";
+$rectangle = new Rectangle(8,6);
+echo $rectangle->area()."\n";
