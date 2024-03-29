3c7ea77a3d91dcb83ccccc3dc4df036ec112e90c
Jennifer <jen1992lo@gmail.com>
2024-03-29 11:32:53 +0100

    Tema 6/Nivel 3/ejercicio 1 añadiendo propiedad faltante

--- Sprint_1/Tema_6/Nivel_3/exercici_1.php
+++ Sprint_1/Tema_6/Nivel_3/exercici_1.php
@@ -17,11 +17,14 @@ class Recurso {
 public $nombre;
 public Tema $tema;
 public Tipo $tipo;
+public $url;
 
-function __construct($nombre,$tema,$tipo){
+function __construct($nombre,$tema,$tipo,$url){
   $this->nombre=$nombre;
   $this->tema=$tema;
   $this->tipo=$tipo;
+  $this->url=$url;
+
 }
 
 function get_nombre(){
@@ -35,10 +38,19 @@ function get_tema(): Tema {
 function get_tipo(): Tipo {
   return $this->tipo;
 }
+
+function get_url(){
+  return $this->url;
+}
+
+function get_info(){
+  $datos = array($this->nombre,$this->tema->name,$this->tipo->name,$this->url);
+  return $datos;
+}
 };
 
-$phpTutorial = new Recurso('PHP para principiantes',Tema::PHP,Tipo::ArticuloWeb);
+$phpTutorial = new Recurso('PHP para principiantes',Tema::PHP,Tipo::ArticuloWeb,'https://www.php.net/manual/es/intro-whatis.php');
 
-print_r("Nombre Recurso: " . $phpTutorial->nombre . "\n")  ;
-print_r("Tema Recurso: " . $phpTutorial->get_tema()->name. "\n") ;
-print_r("Tipo Recurso: " . $phpTutorial->get_tipo()->name. "\n") ;
+foreach($phpTutorial->get_info() as $key => $value){
+  echo $key."-".$value. "\n";
+}
