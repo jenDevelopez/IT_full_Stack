f64e46057e2ee7177509040c2fb0c5ba32f183ea
Jennifer <jen1992lo@gmail.com>
2024-03-29 12:10:07 +0100

    Tema 6/Nivel 3/ejercicio 2 terminado

--- Sprint_1/Tema_6/Nivel_3/exercici_2.php
+++ Sprint_1/Tema_6/Nivel_3/exercici_2.php
@@ -1,2 +1,23 @@
 <?php 
-echo "Hola mundo";
+trait Turbo{
+  function boost(){
+    echo "Se ha iniciado el turbo";
+  }
+}
+class Car{
+  public $marca;
+  public $matricula;
+  public $tipoCombustible;
+  public $velocidadMaxima;
+
+  function __construct($marca ,$matricula,$tipoCombustible,$velocidadMaxima){
+    $this->marca = $marca;
+    $this->matricula = $matricula;
+    $this->tipoCombustible = $tipoCombustible;
+    $this->velocidadMaxima =$velocidadMaxima;
+  }
+  use Turbo;
+}
+
+$coche = new Car('Opel','123XYZ','Diesel',200);
+echo $coche->boost();
